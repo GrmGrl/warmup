49ae619d5bdb3ff317137d6bc9cc5d00d7b08192
GrmGrl <vovahoodoi@gmail.com>
2018-09-15 19:33:27 +0300

    Implement function which converts C to F

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+	if (temperature === +temperature) {
+		return (temperature * 1.8) + 32;
+	} else {
+		throw 'temperature should be a number';
+	}
 };
